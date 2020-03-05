707dd7bba66d92984017e42495d890dede649750
Zhenya Sikirzhitsky <igenexxx@gmail.com>
2020-03-06 00:23:23 +0300

    Adding lazy loading modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CollectionsModule } from './collections/collections.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CollectionsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
