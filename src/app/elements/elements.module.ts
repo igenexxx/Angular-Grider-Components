afbf6c281f8535af5422180399a3e14ef8d6ac43
Zhenya Sikirzhitsky <zhenya_sikirzhitsky@epam.com>
2020-12-05 22:42:22 +0300

    Completed

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ElementsRoutingModule } from './elements-routing.module';
+import { SharedModule } from '../shared/shared.module';
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
+import { PlaceholderComponent } from './placeholder/placeholder.component';
+import { TimesDirective } from './times.directive';
+import { SegmentComponent } from './segment/segment.component';
 
 
 @NgModule({
-  declarations: [ElementsHomeComponent],
+  declarations: [ElementsHomeComponent, PlaceholderComponent, TimesDirective, SegmentComponent],
   exports: [],
   imports: [
     CommonModule,
-    ElementsRoutingModule
+    ElementsRoutingModule,
+    SharedModule,
   ],
 })
 export class ElementsModule { }
---
+++ src/app/mods/accordion/accordion.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-accordion',
+  templateUrl: './accordion.component.html',
+  styleUrls: ['./accordion.component.css']
+})
+export class AccordionComponent implements OnInit {
+  @Input() items = [];
+  openedItemIndex = 0;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onClick(index: number) {
+    this.openedItemIndex = this.openedItemIndex === index ? -1 : index;
+  }
+}
